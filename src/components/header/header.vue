6d116834aa0a11f5d62120eca2974f271edc8e95
Li Dengfeng <lidengfeng9527@163.com>
2019-07-17 20:55:47 +0800

    头部弹框遮罩层完成stick footer布局

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,12 +17,28 @@
                     <span class="text">{{seller.supports[0].description}}</span>
                 </div>
             </div>
-            <div v-if="seller.supports" class="support-count">
+            <div v-if="seller.supports" class="support-count" @click="showDetail">
                 <span class="count">{{seller.supports.length}}个</span>
                 <i class="icon-keyboard_arrow_right"></i>
             </div>
         </div>
-        <div class="bulletin-wrapper"></div>
+        <div class="bulletin-wrapper" @click="showDetail">
+            <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+            <i class="icon-keyboard_arrow_right"></i>
+        </div>
+        <div class="background">
+            <img :src="seller.avatar" width="100%" height="100%">
+        </div>
+        <div v-show="detailShow" class="detail">
+            <div class="detail-wrapper clearfix">
+                <div class="detail-main">
+                   <h1 class="name">{{seller.name}}</h1>
+                </div>
+            </div>
+            <div class="detail-close">
+                <i class="icon-close"></i>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -36,20 +52,29 @@
         },
         data() {
             return {
-                classMap : ['decrease','discount','special','invoice','guarantee']
+                classMap : ['decrease','discount','special','invoice','guarantee'],
+                detailShow:false
             }
         },
         created(){
             
+        },
+        methods:{
+            showDetail(){
+               this.detailShow = true 
+            }
         }
     }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
     @import "../../common/stylus/mixin"
+    
     .header 
+        position:relative;
+        overflow:hidden;
         color:#fff;
-        background-color:#999
+        background-color:rgba(7,17,27,0.5);
         .content-wrapper
             position :relative
             padding:24px 12px 18px 24px
@@ -74,8 +99,11 @@
                 background-color :rgba(0,0,0,0.2)
                 text-align:center
                 .count
-                    font-size:10px    
+                    font-size:10px  
+                    vertical-align :top  
                 .icon-keyboard_arrow_right
+                    margin-left:2px
+                    line-height:24px
                     font-size:10px
             .title
                 margin:2px 0 8px 0
@@ -116,7 +144,71 @@
                         bg-image('special_1')
                 .text
                     line-height:12px
-            
+        .bulletin-wrapper
+            position:relative;
+            height:28px;
+            line-height:28px;
+            padding:0 22px 0 12px;
+            white-space:nowrap;
+            overflow:hidden;
+            text-overflow:ellipsis;   
+            background-color:rgba(7,17,27,0.2);
+            .bulletin-title
+                display:inline-block;
+                width:22px;
+                height:12px;
+                bg-image('bulletin');
+                background-repeat:no-repeat;
+                background-size:22px 12px;
+            .bulletin-text
+                vertical-align :top
+                margin:0 4px;
+                font-size:10px;
+            .icon-keyboard_arrow_right
+                position:absolute;
+                right:12px;
+                top:8px;
+                font-size:10px;
+        .background
+            position:absolute;
+            top:0;
+            left:0;
+            width:100%;
+            height:100%;
+            z-index:-1;
+            filter:blur(10px)
+        .detail
+            position :fixed;
+            top:0;
+            left:0;
+            z-index:100;
+            width:100%;
+            height:100%;
+            overflow:auto;
+            background-color:rgba(7,17,27,0.8);
+            .detail-wrapper
+                width:100%;
+                min-height:100%;
+                .detail-main
+                    margin-top:64px;
+                    padding-bottom:64px;
+                    .name
+                        text-align:center;
+                        font-weight:700;
+                        font-size:16px;
+                        line-height:16px;
+            .detail-close
+                position:relative;
+                width:32px;
+                height:32px;
+                margin:-64px auto 0 auto;
+                clear:both;
+                font-size:32px;
+
+
+
+
+
 
 
 
---
+++ src/components/star/star.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="star" :class="starType">
+        <span v-for="(itemClass,index) in itemClasses" :key="index" :class="itemClass" class="star-item"></span>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            size:{
+                type:Number
+            },
+            scort:{
+                type:Number
+            }
+        },
+        computed:{
+            starType(){
+                return 'star-' + this.size
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+.star
+    .star-item
+        display:inline-block;
+        background-repeat:no-repeat;
+    &.star-48
+    &.star-36
+    &.star-24
+</style>
