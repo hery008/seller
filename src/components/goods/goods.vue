6a9bfe7700e247ae22652284b05108ef49ceeef1
Li Dengfeng <lidengfeng9527@163.com>
2019-07-23 20:45:23 +0800

    购物车布局完成80%，17-14刚开始

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -40,7 +40,7 @@
                 </li>
             </ul>
         </div>     
-        <shopcart></shopcart>     
+        <shopcart :delivery-price="seller.deliveryPrice" :min-price="seller.inPrice"></shopcart>     
     </div>
 </template>
 
--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,15 +1,144 @@
 <template>
-  <div>
-   
+  <div class="shopcart">
+      <div class="content">
+          <div class="content-left">
+            <div class="logo-wrapper">
+                <div class="logo" :class="{'highlight':totalCount>0}">  
+                  <i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
+                </div>
+                <div class="num" v-show="totalCount>0">{{totalCount}}</div>
+            </div>
+            <div class="price" :class="{'highlight':totalPrice>0}">￥ {{totalPrice}} 元</div>  
+            <div class="desc">另需配送费 ￥ {{deliveryPrice}}</div>  
+          </div>
+          <div class="content-right">
+            <div class="pay">
+              {{inPrice}}元起送
+            </div>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
- 
+    props:{
+      selectFoods:{
+        type:Array,
+        default(){
+          return []
+        }
+      },
+      deliveryPrice:{
+        type:Number,
+        default:0
+      },
+      inPrice:{
+        type:Number,
+        default:0
+      },
+    },
+    computed:{
+      totalPrice(){
+        let total = 0
+        this.selectFoods.forEach((food)=>{
+          total += food.price * food.count
+        })
+        return total
+      },
+      totalCount(){
+        let count = 0
+        this.selectFoods.forEach((food)=>{
+          count += food.count
+        })
+        return count
+      }
+    }
 }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+.shopcart
+  height:45px;
+  width:100%;
+  z-index:50;
+  position :fixed;
+  left:0;
+  bottom:0;
+  // background-color:#000;
+  .content
+    display :flex;
+    background :#141d27;
+    color:rgba(255,255,255,0.4);
+    .content-left
+      flex:1;
+      .logo-wrapper
+        display:inline-block;
+        position:relative;
+        top:-10px;
+        margin:0 12px;
+        padding:6px;
+        width:56px;
+        height:56px;
+        box-box-sizing:border-box;
+        vertical-align :top;
+        background:#141d27;
+        border-radius:50%;
+        .num
+          position :absolute;
+          top:0px;
+          right:0px;
+          width:24px;
+          height:16px;
+          line-height:16px;
+          text-align :center
+          border-radius:16px;
+          font-weight:700;
+          font-size:9px;
+          color:#fff;
+          background-color :rgb(240,20,20);
+          box-shadow:0 4px 8px 0 rgba(0,0,0,0.4);
+        .logo
+          width:100%;
+          height:100%;
+          border-radius:50%;
+          background-color :#2b343c;
+          text-align:center;
+          &.highlight
+            background :rgb(0,160,220)
+          .icon-shopping_cart
+            font-size:24px;
+            color:#80858a;
+            line-height:44px;
+            &.highlight
+              color:#fff
+      .price
+        display:inline-block;
+        vertical-align :top;
+        margin-top:12px;
+        line-height:24px;
+        padding-right:12px;
+        box-sizing:border-box;
+        border-right:1px solid rgba(255,255,255,0.1);
+        font-size:16px;
+        font-weight:700;
+        &.highlight
+          color:#fff
+      .desc
+        display:inline-block;
+        vertical-align :top;
+        margin:12px 0 0 12px;
+        line-height:24px;
+        font-size:10px;
+    .content-right
+      flex:0 0 105px;
+      width:105px;
+      .pay
+        height:48px;
+        line-height:48px;
+        font-size:12px;
+        text-align : center
+        font-weight:700;
+        background :#2b333b;
 
 </style>
